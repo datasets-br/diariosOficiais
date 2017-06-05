8c6800caaf51b50c123a8a5dadb73366411b966f
Peter <ppkrauss@gmail.com>
2017-06-05 17:56:43 -0300

    Update step2_carga.sql

--- step2_carga.sql
+++ step2_carga.sql
@@ -11,33 +11,45 @@ CREATE TABLE categorias_tmp (
 COPY categorias_tmp FROM '/tmp/cateorias.csv' CSV HEADER;
 
 
-
 -- autoridades nivel 1
 INSERT INTO oficial.autoridades (name_lex,id_jurisdicao,kx_idpai) 
-  SELECT DISTINCT name_lex,2,10 
+  SELECT DISTINCT 'secretaria;'||name_lex,2,10 
   FROM (SELECT oficial.name2lex(secretaria) as name_lex FROM cateorias_tmp) t
   WHERE name_lex>''
   ORDER BY 1
 ;
 
 -- autoridades nivel 2
-INSERT INTO oficial.autoridades (name,id_jurisdicao,id_pai) 
-  SELECT DISTINCT oficial.name2lex(departamento),2, oficial.autoridade_pai_byname(secretaria)
-  FROM cateorias_tmp WHERE departamento>''
-  ORDER BY 3,1
+INSERT INTO oficial.autoridades (name_lex,id_jurisdicao,kx_idpai) 
+  SELECT 'secretaria;'||sec||';'||name_lex, 2, pai
+  FROM (
+	  SELECT DISTINCT 
+		 oficial.name2lex(departamento) AS name_lex,
+		 oficial.name2lex(secretaria) as sec,
+		 oficial.autoridade_pai_byname(secretaria) as pai
+	  FROM cateorias_tmp 
+  ) t
+  WHERE name_lex>'' AND sec>'' AND sec!=name_lex and name_lex!='?' 
+  ORDER BY pai,1
 ;
 
-
--- categorias nivel 1,seções do DOM-SP
-INSERT INTO oficial.categorias (name,level) 
-  SELECT DISTINCT oficial.name2lex(secaodoc),1
-  FROM categorias_tmp 
-  WHERE secaodoc>'';
+-- categorias nivel 1, seções do DOM-SP
+INSERT INTO oficial.categorias (name_lex,kx_idpai) 
+  SELECT DISTINCT name_lex,1
+  FROM (SELECT oficial.name2lex(secaodoc) as name_lex FROM cateorias_tmp) t
+  WHERE name_lex>'';
 ;
 
--- categorias nivel 2,seções do DOM-SP
-INSERT INTO oficial.categorias (name,level,id_pai) 
-  SELECT DISTINCT oficial.name2lex(secretaria),2, categoria_pai_byname(secaodoc)
-  FROM categorias_tmp 
-  WHERE secretaria>'';
+-- categorias nivel 2, seções do DOM-SP
+INSERT INTO oficial.categorias (name_lex,kx_idpai) 
+  SELECT sec||';'||name_lex, pai
+  FROM (
+	  SELECT DISTINCT 
+		 oficial.name2lex(secretaria) AS name_lex,
+		 oficial.name2lex(secaodoc) as sec,
+		 oficial.categoria_pai_byname(secaodoc) as pai
+	  FROM cateorias_tmp 
+  ) t
+  WHERE name_lex>'' AND sec>'' AND sec!=name_lex and name_lex!='?' 
+  ORDER BY pai,1
 ;
